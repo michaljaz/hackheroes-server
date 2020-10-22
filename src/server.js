f6a3879a9e9e09dcd923c7f608908fabaed38f5d
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2020-10-22 15:40:12 +0200

    naprawa setUserData

--- src/server.js
+++ src/server.js
@@ -1,55 +1,53 @@
-const express = require('express')
-const app = express()
-var db=require("./db.js")
+import NetInfo from "@react-native-community/netinfo";
 
-const port = 3000
 
-//Przyjmowanie requestów logowania
-app.get('/login/', (req, res) => {
-  console.log(`Użytkownik ${req.query.email} loguje się`)
-  db.getUser(req.query.email,req.query.password,(user)=>{
-    if(user==null){
-      res.json({resp:"err"})
-    }else if(user.dataRegistered){
-      res.json({resp:"ok"})
-    }else{
-      res.json({resp:"ok_data"})
-    }
+//callback: true,false
+function IsOnline(callback){
+  NetInfo.fetch().then(resp=>{
+    callback(resp.isConnected)
   })
-})
+}
 
-//Przyjmowanie requestów rejestracji
-app.get('/register/', (req, res) => {
-  console.log(`Użytkownik ${req.query.email} rejestruje się`)
-  db.registerNewUser(req.query.email,req.query.password,(resp)=>{
-    if(resp=="OK"){
-      res.json({resp:"ok"})
-    }else{
-      res.json({resp:"exist"})
-    }
+//callback: "ok","ok_data","err"
+function Login(email,password,callback){
+  fetch(`https://senior-plus.fly.dev/login/?password=${password}&email=${email}`)
+    .then((response) => response.json())
+    .then((json) => {
+      callback(json.resp)
+    })
+}
+
+//callback: "ok","exist"
+function Register(email,password,callback){
+  fetch(`https://senior-plus.fly.dev/register/?password=${password}&email=${email}`)
+  .then((response) => response.json())
+  .then((json) => {
+    callback(json.resp)
   })
-})
+}
 
-//Załadowywanie danych o użytkowniku
-app.get("/get_data/",(req,res)=>{
-  db.getUser(req.query.email,req.query.password,(user)=>{
-    if(user==null){
-      res.json({resp:"err"})
-    }else{
-      res.json({resp:user})
-    }
-  })
-})
+//callback: user,"err"
+function GetUserData(email,password,callback){
+  fetch(`https://senior-plus.fly.dev/get_data/?password=${password}&email=${email}`)
+    .then((response) => response.json())
+    .then((json) => {
+      callback(json.resp)
+    })
+}
 
-//Klient może zapisywać dane
-app.get("/set_data/",(req,res)=>{
-  var user=JSON.parse(req.query.user)
-  db.setUser(user,(resp)=>{
-    res.json({resp})
-  })
-})
+//callback: "ok","err"
+function SetUserData(user,callback){
+  fetch(`https://senior-plus.fly.dev/get_data/?user=${JSON.stringify(user)}`)
+    .then((response) => response.json())
+    .then((json) => {
+      callback(json.resp)
+    })
+}
 
-//Nasłuchiwanie
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+export {
+  Login,
+  Register,
+  IsOnline,
+  GetUserData,
+  SetUserData
+}
